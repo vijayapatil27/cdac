cdbf64b209bca443890e4312420e60a45a547c19
vijayapatil27 <patilvs0619@gmail.com>
2024-09-12 00:29:36 +0530

    Update a2q4.java

--- OOPJ/Assignments/Assignment 2/a2q4.java
+++ OOPJ/Assignments/Assignment 2/a2q4.java
@@ -1,1 +1,36 @@
+//Q4)Write a program that takes a month (1-12) and prints the corresponding season (Winter, 
+Spring, Summer, Autumn) using a switch case
 
+  
+import java.util.*;
+public class a2q4{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.print("enter month: ");
+int month=sc.nextInt();
+switch(month){
+case 1:{
+	System.out.print("winter");
+	break;
+}
+case 2:
+case 3:System.out.print("spring");
+	break;
+case 4:
+case 5:
+case 6:
+case 7:
+case 8:
+System.out.print("summer");
+break;
+case 9:
+case 10:
+System.out.print("autumn");
+break;
+case 11:
+case 12:
+System.out.print("winter");
+	break;
+}	
+	}
+}
