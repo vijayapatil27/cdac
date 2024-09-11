5dbdc4ecd9eef91ae5cbbfa752b6729295afef29
vijayapatil27 <patilvs0619@gmail.com>
2024-09-12 00:27:29 +0530

    Update a2q3.java

--- OOPJ/Assignments/Assignment 2/a2q3.java
+++ OOPJ/Assignments/Assignment 2/a2q3.java
@@ -1,1 +1,17 @@
+//Q3)Write a program that checks if a person is eligible to vote based on their age.
 
+import java.util.*;
+public class a2q3{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.print("enter age: ");
+int age=sc.nextInt();
+if(age>=18){
+	System.out.print("eligible");
+}
+else if(age<0){
+System.out.print("enter valid age");
+}
+else{System.out.print("not eligible");}
+	}
+}
