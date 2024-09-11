aaeb379a960170c8a108d8cb1729a318e4657443
vijayapatil27 <patilvs0619@gmail.com>
2024-09-12 00:23:06 +0530

    Update a2q2.java

--- OOPJ/Assignments/Assignment 2/a2q2.java
+++ OOPJ/Assignments/Assignment 2/a2q2.java
@@ -1,1 +1,25 @@
-
+// Q2)Implement a program that calculates the Body Mass Index (BMI) based on height and weight 
+input using if-else to classify the BMI int categories (underweight, normal weight, 
+overweight,etc)
+  
+import java.util.*;
+import java.lang.*;
+public class a2q2s{
+  
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+System.out.print("enter height in m: ");
+double ht= sc.nextDouble();
+System.out.print("enter weight in kg: ");
+double wt=sc.nextDouble();
+double bmi=wt/Math.pow(ht,2);
+System.out.println("bmi is: "+String.format("%.3f",bmi));
+if(bmi<18){
+	System.out.println("underweight");
+}
+else if(bmi>=18 && bmi<=25){
+	System.out.println("normal weight");
+}
+else {System.out.println("overweight");}
+	}
+}
